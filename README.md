b6494367f20b8d2a3b7831dcab544240631b2bcd
Junha Park <jpark3@scu.edu>
2020-08-29 13:55:38 -0700

    Initial endpoint

---
+++ README.md
@@ -0,0 +1,41 @@
+# WhereToShieldsBadges
+[![Endpoint](https://img.shields.io/website?down_message=offline&label=endpoint&up_message=online&url=https%3A%2F%2Fo3x72k5hu9.execute-api.us-west-1.amazonaws.com%2Fapi)](https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api)
+
+This repository contains source code for custom endpoints used by the [shields.io](https://shields.io/) badges on the [WhereTo](https://github.com/park-junha/WhereTo) README.
+
+## Running the App
+
+This app was developed with Python 3.7.6.
+
+```
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Run the app
+python3 app.py
+```
+
+Optionally, you can create / use a virtual environment to segregate your installed dependencies from your main work environment:
+
+```
+# To create virtual environment (one time only)
+mkdir ~/.virtualenv
+cd ~/.virtualenv
+python3 -m venv WhereToShieldsBadges
+
+# To activate virtual environment (must run every time)
+source ~/.virtualenv/WhereToShieldsBadges/bin/activate
+
+# To install dependencies to your environment (one time only, run above command first)
+cd /path/to/WhereToShieldsBadges
+pip3 install -r requirements.txt
+```
+
+## Badges
+
+The following badges use the custom endpoints:
+
+- [![Chrome](https://img.shields.io/endpoint?url=https%3A%2F%2Fo3x72k5hu9.execute-api.us-west-1.amazonaws.com%2Fapi%2Fchrome)](https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/chrome) - https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/chrome
+- [![Firefox](https://img.shields.io/endpoint?url=https%3A%2F%2Fo3x72k5hu9.execute-api.us-west-1.amazonaws.com%2Fapi%2Ffirefox)](https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/firefox) - https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/firefox
+- [![Safari](https://img.shields.io/endpoint?url=https%3A%2F%2Fo3x72k5hu9.execute-api.us-west-1.amazonaws.com%2Fapi%2Fsafari)](https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/safari) - https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/safari
+- [![Edge](https://img.shields.io/endpoint?url=https%3A%2F%2Fo3x72k5hu9.execute-api.us-west-1.amazonaws.com%2Fapi%2Fedge)](https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/edge) - https://o3x72k5hu9.execute-api.us-west-1.amazonaws.com/api/edge
---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+api = Api(app)
+
+URL_CHROME = 'https://chrome.google.com/webstore/detail/where-to/kdhcodpjaffhbbphkahnkbllddjihima'
+URL_FIREFOX = 'https://addons.mozilla.org/en-US/firefox/addon/where-to/'
+
+COLOR_OK = 'brightgreen'
+COLOR_BAD = 'red'
+COLOR_WARN = 'yellow'
+
+def initResponse(label, message = 'available', color = COLOR_OK):
+  response = {
+    'schemaVersion': 1,
+    'label': label,
+    'message': message,
+    'color': color
+  }
+  return response
+
+class Chrome(Resource):
+  def get(self):
+    html = requests.get(URL_CHROME)
+    if html.status_code is not 200:
+      res = initResponse('chrome', 'unavailable', COLOR_BAD)
+      return res, 200
+    soup = BeautifulSoup(html.text, 'html.parser')
+    elements = soup.select('.C-b-p-D-J .C-b-p-D-Xe.h-C-b-p-D-md')
+    if len(elements) is not 1:
+      res = initResponse('chrome', 'unknown ver', COLOR_WARN)
+      return res, 200
+    version = elements[0].decode_contents()
+    res = initResponse('chrome', version)
+    return res, 200
+
+class Firefox(Resource):
+  def get(self):
+    html = requests.get(URL_FIREFOX)
+    if html.status_code is not 200:
+      res = initResponse('firefox', 'unavailable', COLOR_BAD)
+      return res, 200
+    soup = BeautifulSoup(html.text, 'html.parser')
+    elements = soup.select('.DefinitionList .Definition-dd.AddonMoreInfo-version')
+    if len(elements) is not 1:
+      res = initResponse('firefox', 'unknown ver', COLOR_WARN)
+      return res, 200
+    version = elements[0].decode_contents()
+    res = initResponse('firefox', version)
+    return res, 200
+
+class Safari(Resource):
+  def get(self):
+    res = initResponse('safari', 'unavailable', COLOR_BAD)
+    return res, 200
+
+class Edge(Resource):
+  def get(self):
+    res = initResponse('edge', 'unavailable', COLOR_BAD)
+    return res, 200
+
+class Ping(Resource):
+  def get(self):
+    return {'message': 'up'}, 200
+
+class NotFound(Resource):
+  def get(self, content):
+    return {'message': 'The requested resource does not exist.'}, 404
+
+api.add_resource(Chrome, '/chrome')
+api.add_resource(Firefox, '/firefox')
+api.add_resource(Safari, '/safari')
+api.add_resource(Edge, '/edge')
+api.add_resource(Ping, '/')
+api.add_resource(NotFound, '/<path:content>')
+
+if __name__ == '__main__':
+  app.run(host='localhost', port=8080)
